4c437008a4faeec0b459eff18a586e772f0f4b68
jste1n <wi18b027@technikum-wien.at>
2019-10-10 15:29:29 +0200

    Hello world v1

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * a simple hello world program
+ * @author Jan-Philipp Steinwender
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+		// Prints "Hello Git" to the terminal window.
+        System.out.println("Hello Git");
+    }
+
+}
